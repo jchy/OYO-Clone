f0466f9dbabcea989838ad277a0b49356125ab68
Jaswant Chaudhary <91217491+jchy@users.noreply.github.com>
2022-01-12 07:58:33 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to UNIT-4 Construct week</h1>
+      <h2>OYO Clone using react-redux-MUI</h2>
+    </div>
+  );
+}
