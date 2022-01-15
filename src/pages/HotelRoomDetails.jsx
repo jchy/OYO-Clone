61822e4b6bfec5161dfd0acbfda960e23e138553
Jaswant Chaudhary <91217491+jchy@users.noreply.github.com>
2022-01-15 17:47:44 +0530

    card detail

--- src/pages/HotelRoomDetails.jsx
+++ src/pages/HotelRoomDetails.jsx
@@ -1,63 +1,80 @@
 // import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { shallowEqual, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
-import MuiImageSlider from "mui-image-slider";
-import AliceCarousel from "react-alice-carousel";
 import "react-alice-carousel/lib/alice-carousel.css";
+import { Slide } from "react-slideshow-image";
+import "react-slideshow-image/dist/styles.css";
 
+const getDetails = () => {
+  return fetch("https://oyo-server.herokuapp.com/hotel")
+    .then((res) => res.json())
+    .then((res) => {
+      return res;
+    })
+    .catch((res) => {});
+};
+// var data = [];
+var slideImages = [];
 // HOOkS
 function UserDetails() {
-  let { hotelDataArray, isLoading, isError } = useSelector(
-    (state) => state.app,
-    shallowEqual
-  );
+  const [data, setData] = useState([]);
+  const [roomDetails, setRoomDetails] = useState([]);
+  const itemDetails = useParams();
+  useEffect(() => {
+    getDetails().then((res) => {
+      setData(res);
+      setRoomDetails(
+        data?.filter((rooms) => rooms.id === parseInt(itemDetails.id))
+      );
+    });
+    slideImages = [
+      {
+        url: roomDetails[0]?.poster
+        // caption: "Slide 1"
+      },
+      {
+        url: roomDetails[0]?.images[1]
+        // caption: "Slide 2"
+      },
+      {
+        url: roomDetails[0]?.images[2]
+        // caption: "Slide 3"
+      },
+      {
+        url: roomDetails[0]?.images[3]
+        // caption: "Slide 4"
+      }
+    ];
+  }, [itemDetails.id]);
+  console.log(roomDetails);
 
-  hotelDataArray = hotelDataArray.filter((rooms) => rooms.city === "Bangalore");
-  const images = [
-    hotelDataArray[0]?.poster,
-    hotelDataArray[0]?.images[1],
-    hotelDataArray[0]?.images[2],
-    hotelDataArray[0]?.images[3]
-  ];
+  let { isLoading, isError } = useSelector((state) => state.app, shallowEqual);
   if (isLoading) return <div>...loading</div>;
   return (
     <>
-      <AliceCarousel
-        autoPlay
-        autoPlayInterval="3000"
-        style={{ height: "600px", width: "100%" }}
-      >
-        <img
-          src={images[0]}
-          className="sliderimg"
-          alt=""
-          height="600px"
-          width="100%"
-        />
-        <img
-          src={images[1]}
-          className="sliderimg"
-          alt=""
-          height="600px"
-          width="100%"
-        />
-        <img
-          src={images[2]}
-          className="sliderimg"
-          alt=""
-          height="600px"
-          width="100%"
-        />
-        <img
-          src={images[3]}
-          className="sliderimg"
-          alt=""
-          height="600px"
-          width="100%"
-        />
-      </AliceCarousel>
-      <div>{hotelDataArray[0]?.name}</div>
+      <div>
+        <div>
+          <Slide>
+            {slideImages.map((slideImage, index) => (
+              <div className="each-slide" key={index}>
+                <div
+                  style={{
+                    backgroundImage: `url(${slideImage.url})`,
+                    height: "600px",
+                    width: "100%"
+                  }}
+                >
+                  {/* <span>{slideImage.caption}</span> */}
+                </div>
+              </div>
+            ))}
+          </Slide>
+        </div>
+      </div>
+      <div>
+        <h2>giii</h2>
+      </div>
     </>
   );
 }
